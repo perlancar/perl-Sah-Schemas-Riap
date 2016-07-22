7d0d8a23208eeaf3fbbe28965b1512801d6c5d50
perlancar (@pc-office) <perlancar@gmail.com>
2016-07-22 19:47:10 +0700

    Initial commit

---
+++ lib/Sah/Schema/riap/url.pm
@@ -0,0 +1,12 @@
+package Sah::Schema::riap::url;
+
+# DATE
+# VERSION
+
+our $schema = ['url', {
+    summary => 'Riap URL',
+    'x.completion' => ['riap_url'],
+}, {}];
+
+1;
+# ABSTRACT:
---
+++ lib/Sah/Schemas/Riap.pm
@@ -0,0 +1,11 @@
+package Sah::Schemas::Riap;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: Sah schemas related to Riap
+
+=head1 SEE ALSO
+
+L<Riap>
